3dd900b020c617a4f0cbe27568af77613f217800
Shelby Gluck <shelbygluck@gmail.com>
2020-06-15 17:21:56 -0400

    transitioned to vanilla htmlcss from material-ui

--- src/App.js
+++ src/App.js
@@ -1,92 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Grid';
-
-
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
   
-function App() {
-  const classes = useStyles();
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  
+  comopnentDidMount() {
 
-  return (
-    <div id="main">Hello
-    <div className={classes.root}>
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>school name/alias</Paper>
-      </Grid>
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>school website</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>city</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>state</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>zip</Paper>
-      </Grid>
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>Student Population: </Paper>
-      </Grid>
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>Graph 1</Paper>
-      </Grid>
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>Graph 2</Paper>
-      </Grid>
-      <Grid item xs={12}>
-        <Paper className={classes.paper}>Graph 3</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>
-          <Button onClick={() => {alert('save')}} className="button" variant="contained">SAVE</Button>
-        </Paper>
-      </Grid>
-      <Grid item xs={4}>
-      <Paper className={classes.paper}>
-          <Button onClick={() => {alert('download')}} className="button" variant="contained">DOWNLOAD</Button>
-        </Paper>
-      </Grid>
-      <Grid item xs={4}>
-      <Paper className={classes.paper}>
-          <Button onClick={() => {alert('print')}} className="button" variant="contained">PRINT</Button>
-        </Paper>
-      </Grid>
-    </Grid>
-  </div>
-  </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  }
+  render() {   
+    return (
+      <div id="main">
+      <div id="container">
+        <p className="small">school name/alias</p>
+        <p className="small">school website</p>
+         <div className="rowMultiples">
+           <p className="small">city</p>
+           <p className="small">state</p>
+           <p className="small">zip</p>
+         </div>
+         <p className="small">Student Population: </p>
+         <p className="large">Graph 1</p>
+         <p className="large">Graph 2</p>
+         <p className="large">Graph 3</p>
+         <div className="rowMultiples">
+            <button onClick={() => {alert('save')}} className="button">SAVE</button>
+            <button onClick={() => {alert('download')}} className="button">DOWNLOAD</button>
+            <button onClick={() => {alert('print')}} className="button" >PRINT</button>
+         </div>
+      </div>
+      <h2 id="bottomLogo">EDUCATION ANALYTICS</h2>
+      </div>
+  )}
 }
 
-export default App;
+export default App
