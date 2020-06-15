746b4ee5fa577bc91ff0e53d5ee28049e003d8fb
Shelby Gluck <shelbygluck@gmail.com>
2020-06-15 15:56:47 -0400

    wireframe implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Grid';
 
 
   const useStyles = makeStyles((theme) => ({
@@ -25,25 +26,46 @@ function App() {
     <div className={classes.root}>
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        <Paper className={classes.paper}>xs=12</Paper>
+        <Paper className={classes.paper}>school name/alias</Paper>
       </Grid>
-      <Grid item xs={6}>
-        <Paper className={classes.paper}>xs=6</Paper>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>school website</Paper>
       </Grid>
-      <Grid item xs={6}>
-        <Paper className={classes.paper}>xs=6</Paper>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>city</Paper>
       </Grid>
-      <Grid item xs={3}>
-        <Paper className={classes.paper}>xs=3</Paper>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>state</Paper>
       </Grid>
-      <Grid item xs={3}>
-        <Paper className={classes.paper}>xs=3</Paper>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>zip</Paper>
       </Grid>
-      <Grid item xs={3}>
-        <Paper className={classes.paper}>xs=3</Paper>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Student Population: </Paper>
       </Grid>
-      <Grid item xs={3}>
-        <Paper className={classes.paper}>xs=3</Paper>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Graph 1</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Graph 2</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Graph 3</Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+          <Button onClick={() => {alert('save')}} className="button" variant="contained">SAVE</Button>
+        </Paper>
+      </Grid>
+      <Grid item xs={4}>
+      <Paper className={classes.paper}>
+          <Button onClick={() => {alert('download')}} className="button" variant="contained">DOWNLOAD</Button>
+        </Paper>
+      </Grid>
+      <Grid item xs={4}>
+      <Paper className={classes.paper}>
+          <Button onClick={() => {alert('print')}} className="button" variant="contained">PRINT</Button>
+        </Paper>
       </Grid>
     </Grid>
   </div>
