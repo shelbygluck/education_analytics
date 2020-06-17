acf0b8436f2c045b7e8ee164cd2539abe803928e
Shelby Gluck <shelbygluck@gmail.com>
2020-06-17 15:16:37 -0400

    prettier command ran

--- src/App.js
+++ src/App.js
@@ -1,125 +1,142 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-import {Loading} from './loading'
-import html2canvas from 'html2canvas'
-import pdfConverter from 'jspdf'
-import {CSVLink} from "react-csv"
-import organizeInitialData from './utils'
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import axios from "axios"
+import { Loading } from "./loading"
+import html2canvas from "html2canvas"
+import pdfConverter from "jspdf"
+import { CSVLink } from "react-csv"
+import organizeInitialData from "./utils"
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      website: '',
-      alias: '',
-      city: '',
-      state: '',
-      zip: '',
-      size: 0,
-      programsByPercent: {},
-      raceBreakdown: {},
-      inStateTuition: 0,
-      outStateTuition: 0,
-      actScores: {},
-      satScores: {},
-      genCSV: [],
-      programCSV: [],
-      raceCSV: [],
-      testCSV: [],
-      dataLoaded: false,
-    }
-  }
-  
-  async componentDidMount() {
-    const responseMeta = await axios.get('https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=iP5R8AwSIZG19HgI2rwBYb4xwmYIeNYbyNUizpqC')
-    let response = responseMeta.data.results[0]
-    const data = organizeInitialData(response)
+   constructor(props) {
+      super(props)
+      this.state = {
+         name: "",
+         website: "",
+         alias: "",
+         city: "",
+         state: "",
+         zip: "",
+         size: 0,
+         programsByPercent: {},
+         raceBreakdown: {},
+         inStateTuition: 0,
+         outStateTuition: 0,
+         actScores: {},
+         satScores: {},
+         genCSV: [],
+         programCSV: [],
+         raceCSV: [],
+         testCSV: [],
+         dataLoaded: false,
+      }
+   }
 
-    this.setState(data)
-    this.setState({dataLoaded: true})
-    
-    console.log(this.state)
-    }
+   async componentDidMount() {
+      const responseMeta = await axios.get(
+         "https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=iP5R8AwSIZG19HgI2rwBYb4xwmYIeNYbyNUizpqC"
+      )
+      let response = responseMeta.data.results[0]
+      const data = organizeInitialData(response)
+      this.setState(data)
+      this.setState({ dataLoaded: true })
+   }
 
-  saveAsPDF = () => {
-    let input = window.document.getElementById('schoolData')
-    html2canvas(input)
-      .then(canvas => {
-        const imgData = canvas.toDataURL('image/png')
-        const pdf = new pdfConverter('l', 'pt')
-        pdf.addImage(imgData, 'JPEG', 15, 110, 800, 450)
-        pdf.save('ea.pdf')
-      })
-      .catch(err => console.log(err.message))
-  }
+   saveAsPDF = () => {
+      let input = window.document.getElementById("schoolData")
+      html2canvas(input)
+         .then((canvas) => {
+            const imgData = canvas.toDataURL("image/png")
+            const pdf = new pdfConverter("l", "pt")
+            pdf.addImage(imgData, "JPEG", 15, 110, 800, 450)
+            pdf.save("ea.pdf")
+         })
+         .catch((err) => console.log(err.message))
+   }
 
-  render() {   
-    const dataLoaded = this.state.dataLoaded
-    return (
-      <div id="main">
-      {dataLoaded ? (
-        <div id="container">
-        <div id="schoolData">
-          <segment>
-              <p className="small">{this.state.name}</p>
-              <p className="small">{this.state.website}</p>
-            </segment>
-            <segment>
-            <p className="small">{this.state.city}</p>
-            <p className="small">{this.state.state}</p>
-            <p className="small">{this.state.zip}</p>
-            <p className="small">Student Population: {this.state.size}</p>
-            </segment>
-            <segment>
-              <p className="large">Graph 1</p>
-              <p className="large">Graph 2</p>
-              <p className="large">Graph 3</p>
-            </segment>
-            </div>
-            <segment>
-              <p onClick={this.saveAsPDF}  className="button">SAVE</p>
-              <p onClick={() => {alert('print')}} className="button">PRINT</p>
-            </segment>
-            <segment>
-              <p className="downloadBar">DOWNLOAD DATA: 
-              <CSVLink
-                      className="singleDownload"
-                      data={this.state.genCSV}
-                      ref={(r) => this.csvLink = r}
-                      filename={'genData.csv'}
-                      target="_blank"
-                      >GENERAL
-                  </CSVLink>
-                  <CSVLink
-                      className="singleDownload"
-                      data={this.state.raceCSV}
-                      ref={(r) => this.csvLink = r}
-                      filename={'raceEthnData.csv'}
-                      target="_blank"
-                    >RACE/ETHNICITY
-                  </CSVLink>
-                  <CSVLink
-                      className="singleDownload"
-                      data={this.state.programCSV}
-                      ref={(r) => this.csvLink = r}
-                      filename={'programData.csv'}
-                      target="_blank"
-                      >PROGRAMS
-                    </CSVLink>
-                    </p>
-              </segment>
-        </div>
-       ) : <Loading type="balls" color="lightseagreen" />}
-       <footer>
-          <Loading type="bars" color="lightcoral" />
-          <h2 id="bottomLogo">EDUCATION ANALYTICS</h2>
-          <Loading type="bars" color="darkslategray" />
-      </footer>
-      </div>
-  )}
+   render() {
+      const dataLoaded = this.state.dataLoaded
+      return (
+         <div id="main">
+            {dataLoaded ? (
+               <div id="container">
+                  <div id="schoolData">
+                     <segment>
+                        <p className="small">{this.state.name}</p>
+                        <p className="small">{this.state.website}</p>
+                     </segment>
+                     <segment>
+                        <p className="small">{this.state.city}</p>
+                        <p className="small">{this.state.state}</p>
+                        <p className="small">{this.state.zip}</p>
+                        <p className="small">
+                           Student Population: {this.state.size}
+                        </p>
+                     </segment>
+                     <segment>
+                        <p className="large">Graph 1</p>
+                        <p className="large">Graph 2</p>
+                        <p className="large">Graph 3</p>
+                     </segment>
+                  </div>
+                  <segment>
+                     <p onClick={this.saveAsPDF} className="button">
+                        SAVE
+                     </p>
+                     <p
+                        onClick={() => {
+                           alert("print")
+                        }}
+                        className="button"
+                     >
+                        PRINT
+                     </p>
+                  </segment>
+                  <segment>
+                     <p className="downloadBar">
+                        DOWNLOAD DATA:
+                        <CSVLink
+                           className="singleDownload"
+                           data={this.state.genCSV}
+                           ref={(r) => (this.csvLink = r)}
+                           filename={"genData.csv"}
+                           target="_blank"
+                        >
+                           GENERAL
+                        </CSVLink>
+                        <CSVLink
+                           className="singleDownload"
+                           data={this.state.raceCSV}
+                           ref={(r) => (this.csvLink = r)}
+                           filename={"raceEthnData.csv"}
+                           target="_blank"
+                        >
+                           RACE/ETHNICITY
+                        </CSVLink>
+                        <CSVLink
+                           className="singleDownload"
+                           data={this.state.programCSV}
+                           ref={(r) => (this.csvLink = r)}
+                           filename={"programData.csv"}
+                           target="_blank"
+                        >
+                           PROGRAMS
+                        </CSVLink>
+                     </p>
+                  </segment>
+               </div>
+            ) : (
+               <Loading type="balls" color="lightseagreen" />
+            )}
+            <footer>
+               <Loading type="bars" color="lightcoral" />
+               <h2 id="bottomLogo">EDUCATION ANALYTICS</h2>
+               <Loading type="bars" color="darkslategray" />
+            </footer>
+         </div>
+      )
+   }
 }
 
 export default App
--- src/utils.js
+++ src/utils.js
@@ -1,53 +1,60 @@
-
 export default function organizeInitialData(data) {
-    let name = data.school.name
-    if (data.school.alias != null) {name = `${name} - ${data.school.alias}`}
-
-    const website = data.school.school_url
-    const city = data.school.city
-    const state = data.school.state;
-    const zip = data.school.zip
-    const size = data.latest.student.size
-    const programsByPercent = data.latest.academics.program_percentage
-    const raceBreakdown = data.latest.student.demographics.race_ethnicity
-    const  inStateTuition = data.latest.cost.tuition.out_of_state
-    const outStateTuition = data.latest.cost.tuition.in_state
-    const actScores = data.latest.admissions.act_scores
-    const satScores = data.latest.admissions.sat_scores
-
-    const programHeader = Object.keys(programsByPercent)
-    const programAccessors = Object.values(programsByPercent)
-
-    const raceHeader = Object.keys(raceBreakdown)
-    const raceAccessors = Object.values(raceBreakdown)
-
-    const actHeader = Object.keys(actScores)
-    const actAccessors = Object.values(actScores)
-
-    const satHeader = Object.keys(satScores)
-    const satAccessors = Object.values(satScores)
-
-    const genCSV = [ ['name', 'website', 'city', 'state', 'zip', 'size'], [name, website, city, state, zip, size], ]
-    const programCSV = [ [programHeader], [programAccessors] ]
-    const raceCSV = [ [raceHeader], [raceAccessors] ]
-    const testCSV = [ [actHeader, satHeader], [actAccessors, satAccessors] ]
-
-    return {
-        name: name,
-        website: website,
-        city: city,
-        state: state,
-        zip: zip,
-        size: size,
-        programsByPercent: programsByPercent,
-        raceBreakdown: raceBreakdown,
-        inStateTuition: inStateTuition,
-        outStateTuition: outStateTuition,
-        actScores: actScores,
-        satScores: satScores,
-        genCSV: genCSV,
-        raceCSV: raceCSV,
-        programCSV: programCSV,
-        testCSV: testCSV
-    }
-}
+	let name = data.school.name
+	if (data.school.alias != null) {
+	   name = `${name} - ${data.school.alias}`
+	}
+ 
+	const website = data.school.school_url
+	const city = data.school.city
+	const state = data.school.state
+	const zip = data.school.zip
+	const size = data.latest.student.size
+	const programsByPercent = data.latest.academics.program_percentage
+	const raceBreakdown = data.latest.student.demographics.race_ethnicity
+	const inStateTuition = data.latest.cost.tuition.out_of_state
+	const outStateTuition = data.latest.cost.tuition.in_state
+	const actScores = data.latest.admissions.act_scores
+	const satScores = data.latest.admissions.sat_scores
+ 
+	const programHeader = Object.keys(programsByPercent)
+	const programAccessors = Object.values(programsByPercent)
+ 
+	const raceHeader = Object.keys(raceBreakdown)
+	const raceAccessors = Object.values(raceBreakdown)
+ 
+	const actHeader = Object.keys(actScores)
+	const actAccessors = Object.values(actScores)
+ 
+	const satHeader = Object.keys(satScores)
+	const satAccessors = Object.values(satScores)
+ 
+	const genCSV = [
+	   ["name", "website", "city", "state", "zip", "size"],
+	   [name, website, city, state, zip, size],
+	]
+	const programCSV = [[programHeader], [programAccessors]]
+	const raceCSV = [[raceHeader], [raceAccessors]]
+	const testCSV = [
+	   [actHeader, satHeader],
+	   [actAccessors, satAccessors],
+	]
+ 
+	return {
+	   name: name,
+	   website: website,
+	   city: city,
+	   state: state,
+	   zip: zip,
+	   size: size,
+	   programsByPercent: programsByPercent,
+	   raceBreakdown: raceBreakdown,
+	   inStateTuition: inStateTuition,
+	   outStateTuition: outStateTuition,
+	   actScores: actScores,
+	   satScores: satScores,
+	   genCSV: genCSV,
+	   raceCSV: raceCSV,
+	   programCSV: programCSV,
+	   testCSV: testCSV,
+	}
+ }
