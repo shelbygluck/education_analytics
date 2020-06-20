2df36cd6c71f8805eace4c203a88e24acb9d3ab5
Shelby Gluck <shelbygluck@gmail.com>
2020-06-20 12:38:33 -0400

    Updates

---
+++ precache-manifest.60e41d5336ac88a488da4b4093095c1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ebed4738113bf3e12784d1c8728579",
+    "url": "/education_analytics/index.html"
+  },
+  {
+    "revision": "a57d2c8357734eff8b2f",
+    "url": "/education_analytics/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "2bd60404cb545e7c5311",
+    "url": "/education_analytics/static/css/main.c7b22f8e.chunk.css"
+  },
+  {
+    "revision": "a57d2c8357734eff8b2f",
+    "url": "/education_analytics/static/js/2.0638459f.chunk.js"
+  },
+  {
+    "revision": "6f494a75001b7ec5f6d335f09e642a49",
+    "url": "/education_analytics/static/js/2.0638459f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd60404cb545e7c5311",
+    "url": "/education_analytics/static/js/main.5b229f94.chunk.js"
+  },
+  {
+    "revision": "668df8a3bafee6871b07",
+    "url": "/education_analytics/static/js/runtime-main.a1c5ae41.js"
+  }
+]);
